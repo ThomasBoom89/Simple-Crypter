a33a3f9968284dc74af53b3839c72c556051be0a
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-07-01 17:48:29 +0200

    Scrypt: add randomness via salt

--- crypter_test.go
+++ crypter_test.go
@@ -1,7 +1,6 @@
 package simple_crypter
 
 import (
-	"encoding/base64"
 	"testing"
 )
 
@@ -26,34 +25,6 @@ func TestEncryptDecrypt(t *testing.T) {
 		text, err := crypter.Decrypt(c.password, ciphertext)
 		if text != c.text {
 			t.Error("key should be equal: ", text, c.text)
-		}
-	}
-}
-
-func TestGenerateKeyFromPassword(t *testing.T) {
-	cases := []struct {
-		password    string
-		expectedKey string
-	}{
-		{"", "0YYpWPDP6PuvPBkH8boAt9cQsrDpElExheZpmmqMfdY="},
-		{"ßkjdfl*&3er0Ä", "ym2QYwnRBslS96ctEsv0/y9hhCnvlyX9eVzIWHdrcYo="},
-		{"sfjl öißdfj9", "D8ZBUH37Kzi2r2Wf3GtrwqxG3dgcdZI3tUq/W1Iqzo4="},
-		{"ßdlafjklßdjfkljsdklfjkßjdfio2u3879u8f2wjfwe89jf9w", "AtRpc+S2J05yafzR8zPYadOyheWREoWiT76JBNpkd2U="},
-	}
-
-	for _, c := range cases {
-		crypter := New()
-		key, err := crypter.generateKeyFromPassword(c.password)
-		if err != nil {
-			t.Error("should not throw err: ", err)
-		}
-		encodedKey := base64.StdEncoding.EncodeToString(key)
-		if encodedKey != c.expectedKey {
-			t.Error("key should be equal: ", encodedKey, c.expectedKey)
-		}
-
-		if len(key) != 32 {
-			t.Error("key should have a length of 32 byte: ", encodedKey)
 		}
 	}
 }
